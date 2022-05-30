530a95b4713b5f4d22184a7861ec1026a9478268
KkimMinChan <dfmj4520@naver.com>
2022-05-30 18:13:21 +0900

    최종

---
+++ main.js
@@ -0,0 +1,13 @@
+new Swiper('.Our_partners .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 3000
+    }
+})
+
+
+
+
